2d4c1ad965e576a8a9d1efb4ef627f1dfaf11d6a
Katie Sember <katie.sember@circleci.com>
2021-10-25 15:13:23 -0400

    initial push

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='Hello-World',
-    version='0.0.1',
-    author='Lucas Ribeiro',
-    packages=find_packages(),
-    description='Projeto modelo para script Python',
-)
